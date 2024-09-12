5dc56c26d81b28b9c89e1100c55f0e81ec877b35
Magali <maga.bjb@gmail.com>
2024-09-12 18:57:06 -0300

    agregue icono de pestaña del navegador de una pokebola

--- abmPokemon.php
+++ abmPokemon.php
@@ -15,6 +15,7 @@ if (!isset($_SESSION["logueado"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos/estilos.css">
+    <link rel="shortcut icon" href="imagenes/Pokebola.png">
     <title>Pokedex</title>
 
 </head>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos/estilos.css">
+    <link rel="shortcut icon" href="imagenes/Pokebola.png">
     <title>Pokedex</title>
 
 </head>
